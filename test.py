21070f3cb451474376f7df42f2a11849617d75a7
Bryann <bryann@brovia.com>
2018-12-06 17:25:01 +0100

    init

---
+++ test.py
@@ -0,0 +1,35 @@
+session.interact_user_followers([ 'arianagrande'], amount=1000, randomize=True)
+session.interact_user_followers([ 'selenagomez'], amount=1000, randomize=True)
+
+session.interact_user_followers([ 'jeremykohlanta','chakeup','barbaraopsomer','theogordy','lecoindelodie', ], amount=100, randomize=True)
+session.interact_user_followers([ 'jehovinh','gloomysarah','carolinemia_', 'sleepingbeautyytb', 'evamchd','beyourselfytb','ytbmanond','oceanefrc'], amount=200, randomize=True)
+session.interact_user_followers([ 'iamgalla', 'corentinhuard', 'rsimacourbe', 'romaincosta_', 'valhery', 'bastoswithlove', 'diegoelglaoui', 'stefan_tisseyre', 'nicolassimoes', 'edgardlelegant'], amount=200, randomize=True)
+
+
+session.interact_user_followers([ 'tristandefeuilletvang', 'lestudiodanielle', 'jimchapman','atrapenard', 'aline_dessine' ], amount=200, randomize=True)
+session.interact_user_followers([ 'badies.beauties', 'world__baddies', 'baddie.frr','badieseurope', 'postbadfrenchiies', 'baddiies.fr' ], amount=200, randomize=True)
+
+#session.interact_user_followers([ 'jeremykohlanta','chakeup','barbaraopsomer','theogordy','lecoindelodie', ], amount=100, randomize=True)
+##session.interact_user_followers([ 'jehovinh','gloomysarah','carolinemia_', 'sleepingbeautyytb', 'evamchd','beyourselfytb','ytbmanond','oceanefrc'], amount=200, randomize=True)
+#session.interact_user_followers([ 'iamgalla', 'corentinhuard', 'rsimacourbe', 'romaincosta_', 'valhery', 'bastoswithlove', 'diegoelglaoui', 'stefan_tisseyre', 'nicolassimoes', 'edgardlelegant'], amount=200, randomize=True)
+
+#session.interact_user_followers([ 'camillejoun', 'laurievlltfit', 'chloe_tranchant', 'sarah.coulomb', 'coralie_slvt', 'mayliemartini'], amount=200, randomize=True)
+#session.interact_user_followers([ 'emmaprestifilippo', 'elsa_frenchgirl', 'chloe_fit_', 'roxanebust', 'lucilejoseph_', 'capucine_cine'], amount=200, randomize=True)
+#session.interact_user_followers([ 'victoiremua', 'aline_dessine', 'mamzellesooz', 'audmarshmaloo', 'marieandmood', 'mathilde_mu'], amount=200, randomize=True)
+#session.interact_user_followers([ 'jodielapetitefrenchie', 'ludivineoff', 'lilylovesfashion', 'estherjunelife', 'paulinetrrs', 'lisagermaneau'], amount=200, randomize=True)
+session.interact_user_followers([ 'meganvlt', 'julie_eden', 'marina_de_s', 'gaelle_vp', 'thebabooshka'], amount=200, randomize=True)
+session.interact_user_followers([ 'camillejoun', 'laurievlltfit', 'chloe_tranchant', 'sarah.coulomb', 'coralie_slvt', 'mayliemartini'], amount=200, randomize=True)
+
+session.interact_user_followers([ 'lea_spk', 'sabrinaboot_off', 'floorine_mtr' ], amount=300, randomize=True)
+session.interact_user_followers([ 'beauteactive', 'cameliiab', 'kaaayme' ], amount=300, randomize=True)
+session.interact_user_followers([ 'officiel_nass', 'porrovecchiocoralie', 'nicolebernardes' ], amount=300, randomize=True)
+session.interact_user_followers([ 'romanegrandin', 'cecileshannon', 'sam_nounette' ], amount=300, randomize=True)
+
+session.interact_user_followers([ 'tristandefeuilletvang', 'lestudiodanielle', 'jimchapman','atrapenard', 'aline_dessine' ], amount=200, randomize=True)
+
+session.interact_user_followers([ 'camillejoun', 'laurievlltfit', 'chloe_tranchant', 'sarah.coulomb', 'coralie_slvt', 'mayliemartini'], amount=200, randomize=True)
+session.interact_user_followers([ 'emmaprestifilippo', 'elsa_frenchgirl', 'chloe_fit_', 'roxanebust', 'lucilejoseph_', 'capucine_cine'], amount=200, randomize=True)
+session.interact_user_followers([ 'victoiremua', 'aline_dessine', 'mamzellesooz', 'audmarshmaloo', 'marieandmood', 'mathilde_mu'], amount=200, randomize=True)
+session.interact_user_followers([ 'jodielapetitefrenchie', 'ludivineoff', 'lilylovesfashion', 'estherjunelife', 'paulinetrrs', 'lisagermaneau'], amount=200, randomize=True)
+session.interact_user_followers([ 'meganvlt', 'julie_eden', 'marina_de_s', 'gaelle_vp', 'thebabooshka'], amount=200, randomize=True)
+session.interact_user_followers([ 'camillejoun', 'laurievlltfit', 'chloe_tranchant', 'sarah.coulomb', 'coralie_slvt', 'mayliemartini'], amount=200, randomize=True)
